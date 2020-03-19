1246f6f07d79f4ea222e1441c5e75cc1c2b781af
ONLY-yours <1349021570@qq.com>
2020-03-19 22:27:24 +0800

    adapter over

--- app/src/main/java/com/example/reserve/activity/ReserveActivity.java
+++ app/src/main/java/com/example/reserve/activity/ReserveActivity.java
@@ -1,17 +1,52 @@
 package com.example.reserve.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.reserve.R;
+import com.example.reserve.adapter.HomeAdapter;
 import com.example.reserve.base.BaseActivity;
 
+import static com.example.reserve.adapter.HomeAdapter.*;
+
 public class ReserveActivity extends BaseActivity {
+
+    private RecyclerView rvHomeReserve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_reserve);
+
+        rvHomeReserve=findViewById(R.id.rv_homereserve);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        rvHomeReserve.setLayoutManager(manager);
+        HomeAdapter adapter = new HomeAdapter();
+        rvHomeReserve.setAdapter(adapter);
+
+
+        adapter.setmOnItemClickListener(myOnItemClickListener);
+
     }
+
+    private HomeAdapter.OnItemClickListener myOnItemClickListener = new HomeAdapter.OnItemClickListener() {
+        @Override
+        public void onItemClick(View view, int position) {
+            switch (view.getId()){
+
+                case R.id.btn_pay:
+                    startActivity(FinalPayActivity.class);
+                    break;
+                case R.id.view_detial:
+                    showToast("detials");
+                    break;
+
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/example/reserve/adapter/HomeAdapter.java
@@ -0,0 +1,88 @@
+package com.example.reserve.adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.reserve.R;
+import com.example.reserve.activity.FinalPayActivity;
+
+/**
+ * @auther: lijunjie
+ * @createDate: 2020/3/19  20:14
+ * @purpose：
+ */
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+
+    private OnItemClickListener mOnItemClickListener = null;
+
+    public static interface OnItemClickListener {
+        void onItemClick(View view,int position);
+    }
+
+    public void setmOnItemClickListener(HomeAdapter.OnItemClickListener onItemClickListener){
+        this.mOnItemClickListener = onItemClickListener;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, final int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_home_reserve,parent,false);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOnItemClickListener.onItemClick(view,viewType);
+            }
+        });
+        viewHolder.btnPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOnItemClickListener.onItemClick(view,viewType);
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvPrice.setText("¥ 899");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        //这些控件都可以设置监听事件
+        View view;
+        ImageView iv_img;
+        TextView tvDescribe1;
+        TextView tvDescribe2;
+        TextView tvDescribe3;
+        TextView tvPrice;
+        Button btnPay;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            view=itemView.findViewById(R.id.view_detial);
+            iv_img=itemView.findViewById(R.id.iv_img);
+            tvDescribe1=itemView.findViewById(R.id.tvdescribe1);
+            tvDescribe2=itemView.findViewById(R.id.tvdescribe2);
+            tvDescribe3=itemView.findViewById(R.id.tvdescribe3);
+            tvPrice=itemView.findViewById(R.id.tv_price);
+            btnPay=itemView.findViewById(R.id.btn_pay);
+        }
+    }
+
+
+}
